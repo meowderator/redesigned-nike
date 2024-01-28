474ad0348a75a728e1f31c018f85d8c2fa5580bf
meowderator <144629990+meowderator@users.noreply.github.com>
2024-01-28 08:11:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var tl = gsap.timeline();
+
+gsap.set(".a", { opacity: 0, y: 10})
+gsap.set("#right #mainimg", {opacity: 0, y: 10, scale: 1.6})
+tl
+    .from("#left", {
+        width: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5
+    })
+    .from("#right", {
+        width: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5
+    })
+    .to(".a", {
+        delay: -2,
+        stagger: .2,
+        opacity: 1,
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 2
+    })
+    .to("#right #mainimg", {
+        delay: -1,
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        ease: Expo.easeInOut,
+        duration: 2
+    })
